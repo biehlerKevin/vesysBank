59a401dbad1981601394708ba101b9367e5f7992
Kevin Biehler <kevin_biehler@hotmail.com>
2016-05-04 20:19:59 +0200

    completed RMI task, would be nicer with IF BankDriver2

--- bankVesys/src/rmi/RMIServer.java
+++ bankVesys/src/rmi/RMIServer.java
@@ -1,14 +1,27 @@
 package rmi;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+
+import soap.MyBank;
 
 public class RMIServer {
 
-
 	public static void main(String args[]) throws Exception {
-	BankRMIService b = new BankRMIServiceImpl();
-	Naming.bind("BankRMIService", b);
+		
+		try {
+			LocateRegistry.createRegistry(1099);
+			}
+			catch (RemoteException e) {
+			System.out.println(">> registry could not be exported");
+			System.out.println(">> probably another registry already runs on 1099");
+			}
+		BankRMIService b = new BankRMIServiceImpl(new MyBank());
+		Naming.bind("BankRMIService", b);
+		
+		System.out.println("BankRMI started");
 	}
-	
-	
+
 }
--- bankVesys/src/rmi/TestClient.java
+++ bankVesys/src/rmi/TestClient.java
@@ -3,12 +3,13 @@ package rmi;
 import java.rmi.Naming;
 
 public class TestClient {
-	
+
 	public static void main(String[] args) throws Exception {
-		BankRMIService b = (BankRMIService)Naming.lookup(
-		"rmi://localhost/BankRMIService");
-		
-		
+		BankRMIService b = (BankRMIService) Naming.lookup("rmi://localhost/BankRMIService");
+
+		System.out.println(b.createAccount("kevin"));
+		b.deposit("0", 100);
+		System.out.println(b.getBalance("0"));
 	}
 
 }
